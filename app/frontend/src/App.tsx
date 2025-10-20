f543821847dead2a37061d2fb71c5a3af6b77629
dv20fnh <dv20fnh@cs.umu.se>
2025-10-20 13:59:08 +0200

    removed backend

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,11 +1,24 @@
 import './App.css'
-
+import { useEffect, useState } from 'react';
 function App() {
+
+  const [message, setMessage] = useState<string>("Loading...");
+
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/hello")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message))
+      .catch((err) => {
+        console.error("Error fetching:", err);
+        setMessage("Failed to connect to backend");
+      });
+  }, []);
 
   return (
     <>
       <div className='bg-blue-900 w-12 h-20'>
-
+        <p>{message}</p>
       </div>
     </>
   )
