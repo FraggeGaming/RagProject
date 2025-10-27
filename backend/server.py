32ddd2bd31073b77e4a17c2262ea10f4f69c6d7c
dv20fnh <dv20fnh@cs.umu.se>
2025-10-27 19:25:49 +0100

    finilized the backend

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 from flask import Flask, jsonify, request
+from pgvector import Vector
 import psycopg2
 from flask_cors import CORS
 
@@ -9,6 +11,7 @@ from langchain_core.output_parsers import StrOutputParser
 from sentence_transformers import SentenceTransformer, util
 
 from pgvector.psycopg2 import register_vector
+from flask import Response
 
 
 app = Flask(__name__)
@@ -26,31 +29,49 @@ llm = ChatOllama(
 model_path = "ibm-granite/granite-embedding-english-r2"
 model = SentenceTransformer(model_path)
 
-def GenerateLlmResponse(llm, msg, docs):
+def GenerateLlmResponse(llm, text):
     prompt = PromptTemplate(
-        template="""You are an assistant for summarizing text from upcoming events.
-        Your task is to use the following documents and describe them to the user in maximum three sentences. 
-        Keep it concise.
-        If you don't know the answer, just say that you don't know:
-        Question: {msg}
-        Documents: {docs}
-        Answer:
-        """,
-        input_variables=["msg", "docs"],
-        )
+        template=(
+            "{text}"
+        ),
+        input_variables=["text"],
+    )
 
     rag_chain = prompt | llm | StrOutputParser()
-  
-    response = rag_chain.invoke(
-        {"msg": msg,
-        "docs": docs}
-    )
-    print(f"{response}")
+    response = rag_chain.invoke({"text": text})
+    print(response)
     return response
 
 
-def fetchSimilarEvents(msg):
+def fetchSimilarEvents(msg, dates):
     embedding = model.encode(msg)
+    if hasattr(embedding, "tolist"):
+        embedding = embedding.tolist()
+    embedding = Vector(embedding)
+
+
+    start, end = None, None
+    if dates:
+        if len(dates) > 0:
+            start = dates[0]
+        if len(dates) > 1:
+            end = dates[1]
+
+    where_clause = ""
+    params = []
+
+    if start and end:
+        where_clause = "WHERE event_date BETWEEN %s::date AND %s::date"
+        params.extend([start, end])
+    elif start:
+        where_clause = "WHERE event_date = %s::date"
+        params.append(start)
+    elif end:
+        where_clause = "WHERE event_date = %s::date"
+        params.append(end)
+        
+    print("WHERE:", where_clause)
+    print("PARAMS:", params, "TYPES:", [type(p) for p in params])
     with psycopg2.connect(
         dbname="rag",
         user="postgres",
@@ -60,31 +81,66 @@ def fetchSimilarEvents(msg):
     ) as conn:
         register_vector(conn)
         with conn.cursor() as cur:
-            # 3. Query for 3 most similar embeddings
-            cur.execute("""
-            SELECT title, full_desc, place
-            FROM events
-            ORDER BY embedding <-> %s
-            LIMIT %s;
-        """, (embedding, 3))
+            cur.execute(f"""
+                SELECT title, full_desc, place, event_date, link
+                FROM events
+                {where_clause}
+                ORDER BY embedding <-> %s
+                LIMIT 2;
+            """, (*params, embedding))
             results = cur.fetchall()
+
     return results
 
 @app.route('/fetchAll', methods=['GET'])
 def fetch_all():
     try:
+        #If only one date is selected, then that date is in Date-Start
+        #If range is selected, range is in Date-Start and Date-End
         message = request.headers.get('Message')
+        startDate = request.headers.get('Date-Start')
+        endDate = request.headers.get('Date-End')
         
-        vectorStore = fetchSimilarEvents(message)
-        print(message)
-        
-        
-        response = GenerateLlmResponse(llm, message, vectorStore)
+        dateList = []
+        dateList.append(datetime.fromisoformat(startDate).date())
+        if(endDate):
+            dateList.append(datetime.fromisoformat(endDate).date())
+            
+        if len(dateList) == 2:
+               
+            d1 = dateList[0]
+            d2 = dateList[1]
+
+            if d1 and d2 and d1 > d2:
+                dateList = [dateList[1], dateList[0]]
+            
+        vectorStore = fetchSimilarEvents(message, dateList)
+        enriched_entries = []
+
+        for title, full_desc, place, event_date, link in vectorStore:
+            item_context = f"Title: {title}\nDescription: {full_desc}\nLocation: {place}"
+            
+            llm_description = GenerateLlmResponse(
+                llm,
+                f"Provide a concise and engaging description for the following event:\n\n{full_desc}"
+            )
+            
+            enriched_entry = f"{title}\n{llm_description}\n{place}\n{event_date}\n{link}"
+            enriched_entries.append(enriched_entry)
+            enriched_entries.append("\n--------------------------\n")
 
 
+        r = "\n\n".join(enriched_entries)
+        print(r)
+        
+        if not r.strip():  
+            response = "No events found"
+        else:
+            response = r
 
-        # Return JSON
-        return jsonify(response)
+        
+        
+        return jsonify({"text": response})
 
     except Exception as e:
         print("Error:", e)
